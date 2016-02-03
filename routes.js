160da6851ac032338b5bedb1ec1ade3c0ec9c493
Tal Ben-Ari <talbenari1@gmail.com>
2016-02-02 16:53:17 -0800

    Added CSS support to layout.html

--- routes.js
+++ routes.js
@@ -3,7 +3,8 @@ module.exports = (app, logger) => {
   // The main page
   app.get('/', (req, res) => {
     res.render('index.html', {
-      'title': 'Join'
+      'title': 'Join',
+      'css': []
     });
   });
 };

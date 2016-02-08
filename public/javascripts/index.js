6e26162ff18397f58bebfe4949aff5c6f5fd23d1
Jacob Young <jacob@jry.io>
2016-02-08 01:13:27 -0800

    Match Standard JS syntax

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,24 +1,25 @@
-$(function() { 
-  window.addEventListener('submit', function(e) {
-    e.preventDefault();
-    var inputs = document.querySelectorAll('input[data-required]');
-    Array.prototype.slice.call(inputs).forEach(function(el) {
+/* globals $ */
+
+$(function () {
+  window.addEventListener('submit', function (e) {
+    e.preventDefault()
+    var inputs = document.querySelectorAll('input[data-required]')
+    Array.prototype.slice.call(inputs).forEach(function (el) {
       if (el.getAttribute('pattern') ? !(new RegExp(el.getAttribute('pattern')).test(el.value)) : el.value === '') {
-        e.stopPropagation();
-        el.setAttribute('required', true);
-      }
-      else {
+        e.stopPropagation()
+        el.setAttribute('required', true)
+      } else {
         $.ajax({
           url: '/join',
           type: 'POST',
           data: $('form').serialize()
-        }).done(function(data) {
-          document.querySelector('.overlay-container').style.display = 'block';
-          document.querySelector('#join-button').style.display = 'none';
+        }).done(function (data) {
+          document.querySelector('.overlay-container').style.display = 'block'
+          document.querySelector('#join-button').style.display = 'none'
           // To clear fields, so no annoying closing messages displayed by browser
-          document.querySelector('form').reset(); 
-        });
+          document.querySelector('form').reset()
+        })
       }
-    });
-  });
-});
+    })
+  })
+})

488f67c84c64e3511fcdbeca01e596d6d57647da
Jacob Young <jacob@jry.io>
2016-02-07 18:37:39 -0800

    Add mongoose models

---
+++ models/survey.js
@@ -0,0 +1,12 @@
+let mongoose = requre('mongoose');
+let Schema = mongoose.Schema;
+
+let surveySchema =  new Schema({
+  "experience": [String],
+  "interests": [String],
+  "more-interests": String,
+  "projects": [String]
+  "more-projects": [String],
+  "events": [String],
+  "more-events": [String]
+});

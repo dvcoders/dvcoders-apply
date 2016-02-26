0e1ccdc36a279eecaff3658ba72cbce11a9004b6
jsong <jsong@SongJungwooui-MacBook-Pro.local>
2016-02-26 00:57:46 -0800

    Follow JavaScript Standard Style

--- test.js
+++ test.js
@@ -1,26 +1,26 @@
-var app = require('./app.js').app;
-var request = require('supertest')(app);
+var app = require('./app.js').app
+var request = require('supertest')(app)
 
-describe('loading express', function() {
-	it('GET /', function testSlash(done) {
-		request.get('/').expect(200, done);
-	});
-	it('404 test', function testPath(done) {
-		request.get('/foo/bar').expect(404, done);
-	});
-	it('post to /join', function testJoin(done) {
-		request
-			.post('/join')
-			.send({
-				'firstName':'John',
-				'lastName':'Smith',
-				'email':'fjaiower@boier.com',
-				'mailchimp':'on',
-				'githubUsername':'', // TODO: test addToTeam()
-				'more-interests':'',
-				'more-projects':'',
-				'more-events':''
-			})
-			.expect(200, done);
-	});
-});
+describe('loading express', function () {
+  it('GET /', function testSlash (done) {
+    request.get('/').expect(200, done)
+  })
+  it('404 test', function testPath (done) {
+    request.get('/foo/bar').expect(404, done)
+  })
+  it('post to /join', function testJoin (done) {
+    request
+      .post('/join')
+      .send({
+        'firstName': 'John',
+        'lastName': 'Smith',
+        'email': 'fjaiower@boier.com',
+        'mailchimp': 'on',
+        'githubUsername': '', // TODO: test addToTeam()
+        'more-interests': '',
+        'more-projects': '',
+        'more-events': ''
+      })
+      .expect(200, done)
+  })
+})

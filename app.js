b0065d7ff654d0c6b6d50b453ccea9ad850d6e1b
jsong <jsong@SongJungwooui-MacBook-Pro.local>
2016-02-27 05:01:35 -0800

    Added a transport to slack in winston. Now, error logs are sent to Slack #general channel as well. I tried to use winston-slack module, but they are broken now and not maintaining the code. Thus, I got it fixed and included here.

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ 'extended': true }))
 app.use(bodyParser.json())
 
 // Create the logging utility
-let logger = require('./logger.js')()
+let logger = require('./logger/logger.js')()
 
 // Pass express data through the logger
 app.use(require('morgan')('dev', {
@@ -47,7 +47,7 @@ if (config.github.apiKey && config.github.userAgent && config.slack.token) {
 }
 
 // Connect to MongoDB and start web server on success
-db(err => {
+db((err) => {
   if (err) {
     return console.error('MongoDB connection error:', err)
   }

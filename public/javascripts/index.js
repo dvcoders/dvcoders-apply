e7b6615a1802ecbecacfda87c7c5e94e74f36488
Jesse Smick <janka102@gmail.com>
2016-10-19 00:24:43 -0700

    Switch a bit to jQuery from vanilla

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,14 +6,19 @@ function scrollToElement (element) {
 }
 
 function renderError (el, message) {
-  console.log('Appending: ' + message)
-  el.text(' - ' + message)
+  console.log(message)
+  el.text(message)
+  el.css({
+    color: '#d35400',
+    display: 'inline'
+  })
 }
 
 $(function () {
   window.addEventListener('submit', function (e) {
     e.preventDefault()
     var requiredMatched = true
+    var form = $('form')
     var inputs = $('input[data-required]')
     var githubUsername = $('#githubUsername').get(0)
 
@@ -31,14 +36,14 @@ $(function () {
 
     if (requiredMatched) {
       $.ajax({
-        url: '/join',
-        type: 'POST',
-        data: $('form').serialize()
+        url: form.attr('action'),
+        type: form.attr('method'),
+        data: form.serialize()
       }).then(function (data) {
         $('.overlay-container').show()
         $('#join-button').hide()
         // To clear fields, so no annoying closing messages displayed by browser
-        document.querySelector('form').reset()
+        form.get(0).reset()
       }, function (res) {
         var data = JSON.parse(res.responseText)
 
